12e88512024db8dd877846219084a1c4b89eac0d
William Kennedy <alexkenned@gmail.com>
2020-02-20 21:09:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,29 @@
 def base_hash 
-  :railroads=>
+  :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces =>4,
+    :rent_in_dollars =>
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
+      :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
+      :four_pieces_owned =>200
     },
-    :names=>
+    :names =>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
         "mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
         "mortgage_value"=>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
         "mortgage_value"=>"$400"
       },
-      :shortline=>
+      :shortline =>
       {
         "mortgage_value"=>"$800"
       }
@@ -36,31 +36,31 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  :railroads=>
+ :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces =>4,
+    :rent_in_dollars =>
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
+      :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
+      :four_pieces_owned =>200
     },
-    :names=>
+    :names =>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
         "mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
         "mortgage_value"=>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
         "mortgage_value"=>"$400"
       },
-      :shortline=>
+      :shortline =>
       {
         "mortgage_value"=>"$800"
       }
@@ -73,31 +73,31 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  :railroads=>
+  :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces =>4,
+    :rent_in_dollars =>
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
+      :one_piece_owned =>25,
+      :two_pieces_owned =>50,
+      :three_pieces_owned =>100,
+      :four_pieces_owned =>200
     },
-    :names=>
+    :names =>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
         "mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
         "mortgage_value"=>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
         "mortgage_value"=>"$400"
       },
-      :shortline=>
+      :shortline =>
       {
         "mortgage_value"=>"$800"
       }
